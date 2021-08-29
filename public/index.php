9710ade5da8c53a9185de826afe8741a0df80eea
rgabasa <rgabasa.mynimo@gmail.com>
2021-08-30 00:33:30 +0800

    added header

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
