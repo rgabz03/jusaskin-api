64bb442b28c392fa1e6577443e065a188ddba2e6
rgabasa <rgabasa.mynimo@gmail.com>
2021-08-30 14:13:41 +0800

    remove cors

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
